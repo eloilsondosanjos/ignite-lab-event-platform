b3644fd4831c4d10903108223b539ec330b449ef
Eloilson <eloilson.ageedu@gmail.com>
2022-06-26 07:46:45 -0300

    fix: lesson props

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -4,13 +4,20 @@ import ptBR from "date-fns/locale/pt-BR";
 import { Link, useParams } from "react-router-dom";
 import classNames from "classnames";
 
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: "live" | "class";
+}
+
 export function Lesson(props: LessonProps) {
   const { slug } = useParams<{ slug: string }>();
 
   const isLessonAvailable = isPast(props.availableAt);
   const availableDateFormatted = format(
     props.availableAt,
-    "EEEE' • ' d ' de ' MMMM ' • ' k 'h' mm ' ",
+    "EEEE' • ' d ' de ' MMMM ' • ' k 'h' mm",
     {
       locale: ptBR,
     }
